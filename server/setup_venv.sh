cae0b1dc632bab8a594b499e6c012128363786b8
Roger Yuan <49228924+RogerNoROG@users.noreply.github.com>
2025-08-20 10:50:49 +0800

    Update setup_venv.sh

--- server/setup_venv.sh
+++ server/setup_venv.sh
@@ -6,7 +6,7 @@ if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
 source venv/bin/activate
-pip config set global.index-url https://mirror.nju.edu.cn/pypi/web/simple
+#pip config set global.index-url https://mirror.nju.edu.cn/pypi/web/simple
 pip install --upgrade pip
 pip install -r requirements.txt
 
